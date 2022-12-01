c28794591d45d60db69eed757dff7d693d5ebef1
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-02 01:58:02 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            // Kodlarını Buraya gir. Değerlerini oku. Çıktısını ekrana yaz. Sınıfının isminin solution olması tavsiye ediyoruz.
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                LetsReview(s);
+            }
+
+            static void LetsReview(string s)
+            {
+                string evenStr = "";
+                string oddStr = "";
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        evenStr += s[i];
+                    }
+                    else
+                    {
+                        oddStr += s[i];
+                    }
+                }
+
+                Console.WriteLine($"{evenStr} {oddStr}");
+            }
+        }
+    }
+}
